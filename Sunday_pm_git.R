979a5712ae01e1b2896105a607da8672f4fa23ab
diwashJangam <diwash.jangam@mavs.uta.edu>
2015-04-19 14:54:09 -0500

    population mean

--- Sunday_pm_git.R
+++ Sunday_pm_git.R
@@ -2,13 +2,32 @@
 #Diwash Jangam
 # #diwash.jangam@mavs.uta.edu
 #Apr 19th 2015
-
+#installed
 #Install packages
 install.packages("dplyr", dependencies=TRUE)
 
 #first you need to load after you install, this line needs to be here.
 library("dplyr")
 
+
 # read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep="\t", header=TRUE)
 
+# %>% in equivilent to | in shell.using dplyr
+gap.in %>% 
+  filter(pop>15000000) %>%
+  select(country,year,pop) %>%
+  group_by(country)%>%
+  summarize(min=min(pop))
+
+#calculate mean population per continent across per years prior to 1990
+gap.in %>% 
+  group_by(continent, year)%>%
+  filter(year<1990)%>%
+  summarize(mean(pop))%>%
+#print the full length data in the console.
+  as.data.frame()
+
+
+
+  
